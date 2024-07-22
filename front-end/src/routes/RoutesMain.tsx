39a8ae9c074f85bb581721dcd9198bfe143e8836
João Filho <joaofilhomil@gmail.com>
2024-07-22 16:16:39 -0300

    :sparkles: feat: Adicionar arquivo de rotas principais

---
+++ front-end/src/routes/RoutesMain.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const MainRoutes: React.FC = () => {
+  return (
+    <Routes>
+      
+    </Routes>
+  );
+};
+
+export default MainRoutes;
